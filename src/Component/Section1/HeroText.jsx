9dd3b5100622aaae67ae8407a0b17a7102ca3ba6
soumyachoudhary339-eng <soumyachoudhary339@gmail.com>
2026-03-16 22:28:57 +0530

    for check switch fn

--- src/Component/Section1/HeroText.jsx
+++ src/Component/Section1/HeroText.jsx
@@ -5,6 +5,7 @@ const HeroText = () => {
     <div className='p-8'>
          <h3 className=' mb-7 text-5xl font-bold '>Prospective <br /><span className='text-gray-500' >customer</span> <br /><span>segmentation</span></h3>
          <p className='text-xm font-medium text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, sit natus perspiciatis corporis aperiam qui incidunt quod vel obcaecati minus.</p>
+          <button className='mt-5 rounded-md bg-blue-500 px-4 py-2 text-white'>Get Started</button>
       </div>
   )
 }
